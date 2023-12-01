7560ed8b0237fecd1d3b72a906574d25add28df6
rosso101 <victorprivv101@gmail.com>
2023-12-01 09:58:00 +0300

    completed assignment

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,36 @@
 def write_file(file_name, file_content):
-    pass
+    """
+    Write content to a text file.
 
-def append_file(file_name, append_content):
-    pass
+    Args:
+        file_name (str): The name of the file (including file extension).
+        file_content (str): The content to be written to the file.
+    """
+    with open(f"{file_name}.txt", "w") as file:
+        file.write(file_content)
+
+
+def append_to_file(file_name, append_content):
+    """
+    Append content to an existing text file.
+
+    Args:
+        file_name (str): The name of the file (including file extension).
+        append_content (str): The content to be appended to the file.
+    """
+    with open(f"{file_name}.txt", "a") as file:
+        file.write(append_content)
+
 
 def read_file(file_name):
-    pass
+    """
+    Read content from a text file.
+
+    Args:
+        file_name (str): The name of the file (including file extension).
+
+    Returns:
+        str: The content read from the file.
+    """
+    with open(f"{file_name}.txt", "r") as file:
+        return file.read()
